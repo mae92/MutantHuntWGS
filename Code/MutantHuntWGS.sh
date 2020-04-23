d6652f6c6316ae2560f6af3a81493a13c339ba19
Mitch Ellison <mae92@pitt.edu>
2020-04-23 19:44:34 -0400

    Add files via upload

--- Code/MutantHuntWGS.sh
+++ Code/MutantHuntWGS.sh
@@ -265,7 +265,7 @@ do
 
 	#Use SNPeff to find out some more information about these SNPs like the amino acid change in the resulting protein
 
-	java -Xmx4G -jar /Main/snpEff/snpEff.jar ann -v Saccharomyces_cerevisiae "$VCF_FILE" > "$OUTPUT_FILE"/SNPeff_Output/"$VCF_NAME"/SNPeff_Annotations.vcf &> /dev/null
+	java -Xmx4G -jar /Main/snpEff/snpEff.jar ann -v Saccharomyces_cerevisiae "$VCF_FILE" > "$OUTPUT_FILE"/SNPeff_Output/"$VCF_NAME"/SNPeff_Annotations.vcf
 done
 
 
@@ -283,7 +283,7 @@ do
 
 	##Run SIFT
 
-	java -Xmx4G -jar /Main/SIFT4G_Annotator.jar -c -i "$VCF_FILE"  -d /Main/EF4.74 -r "$OUTPUT_FILE"/SIFT_Output/"$VCF_NAME" &> /dev/null
+	java -Xmx4G -jar /Main/SIFT4G_Annotator.jar -c -i "$VCF_FILE"  -d /Main/EF4.74 -r "$OUTPUT_FILE"/SIFT_Output/"$VCF_NAME"
 
 done
 
